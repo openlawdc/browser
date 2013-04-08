8c62579e033365791d22ecdc561aad345969516d
Tom MacWright <tom@macwright.org>
2013-04-08 17:51:30 -0400

    Support sections

--- js/browser.js
+++ js/browser.js
@@ -21,7 +21,42 @@ d3.json('index.json', function(err, index) {
         sectionsFor(d);
     }
 
+    var s = d3.select('#section');
+    var subsections = d3.select('.sections');
+
+    function doSection(d) {
+        d3.json('sections/' + d[0].replace('§ ', '').trim() + '.json', function(err, section) {
+            var s = d3.select('#section');
+            s.select('h1').text(section.heading.catch_text);
+            s.select('.text').text(section.text);
+
+            var sections = subsections.selectAll('section')
+                .data(section.sections, function(d) {
+                    return d.prefix + d.text;
+                });
+
+            sections.enter().append('section');
+
+            sections.exit().remove();
+
+            sections.html(function(d) {
+                return '<strong>' + d.prefix + '</strong> ' + d.text;
+            });
+        });
+    }
+
     function sectionsFor(title) {
+
+        function clickSection(d) {
+            var t = this;
+            sections
+                .classed('active', function(d) {
+                    return this == t;
+                });
+
+            doSection(d);
+        }
+
         var sections = d3.select('#sections')
             .selectAll('li.section')
             .data(index.sections.filter(function(s) {
@@ -40,6 +75,7 @@ d3.json('index.json', function(err, index) {
             .attr('class', 'section')
             .text(function(d) {
                 return d[0] + ' ' + d[1];
-            });
+            })
+            .on('click', clickSection);
     }
 });
