0a8520041ce401125139dda8c63cb7b08bf5677a
Eric Mill <konklone@gmail.com>
2013-04-16 21:57:58 -0400

    Adding in basic DCR linkage, linking to landing page for volumes 57 and above

--- js/browser.js
+++ js/browser.js
@@ -198,13 +198,18 @@ d3.json('index.json').on('load', function(index) {
     function cited(text) {
         return Citation.find(text, {
             context: {dc_code: {source: "dc_code"}},
-            types: ["dc_code", "law", "stat"],
+            types: ["dc_code", "dc_register", "law", "stat"],
             replace: function(cite) {
                 if (cite.type == "dc_code")
                     return "<a href=\"" + codeUrlFor(cite) + "\">" + cite.match + "</a>";
                 else if (cite.type == "law")
                     return "<a href=\"" + lawUrlFor(cite) + "\">" + cite.match + "</a>";
-                else if (cite.type == "stat") {
+                else if (cite.type == "dc_register") {
+                    if (parseInt(cite.dc_register.volume, 10) >= 57)
+                        return "<a href=\"" + dcrUrlFor(cite) + "\">" + cite.match + "</a>";
+                    else
+                        return cite.match;
+                } else if (cite.type == "stat") {
                     if (parseInt(cite.stat.volume, 10) >= 65)
                         return "<a href=\"" + statUrlFor(cite) + "\">" + cite.match + "</a>";
                     else
@@ -230,6 +235,12 @@ d3.json('index.json').on('load', function(index) {
 
     function statUrlFor(cite) {
         return "http://api.fdsys.gov/link?collection=statute&volume=" + cite.stat.volume + "&page=" + cite.stat.page;
+    }
+
+    // just link to that year's copy on the DC Register website
+    function dcrUrlFor(cite) {
+        var year = parseInt(cite.dc_register.volume, 10) + 1953;
+        return "http://www.dcregs.dc.gov/Gateway/IssueList.aspx?IssueYear=" + year;
     }
 
     function sectionsFor(title) {
