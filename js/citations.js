fd8fe9984fa0786bc30d1a6df1654cb9edec3b6f
Eric Mill <konklone@gmail.com>
2013-06-01 12:11:09 -0400

    Got DC Law links working, with a global lookup

--- js/citations.js
+++ js/citations.js
@@ -6,17 +6,69 @@ function cited(text) {
             }
         },
         excerpt: 40,
-        types: ['dc_code', 'dc_register', 'law', 'stat'],
+        types: ['dc_code', 'dc_register', 'dc_law', 'law', 'stat'],
         replace: {
             dc_code: codeCited,
             law: lawCited,
             dc_register: dcrCited,
-            stat: statCited
+            stat: statCited,
+            dc_law: dcLawCited
         }
     }).text;
 
     function linked(url, text) {
         return "<a href='" + url + "'>" + text + "</a>";
+    }
+
+    // The detected citation is a DC "Law" - a slip law number, assigned
+    // to an act after it becomes law.
+    //
+    // However, DC's legislative portal, LIMS, does not know law numbers,
+    // only act numbers. @vdavez and @adamturoff created a crosswalk between
+    // the two.
+    //
+    // The crosswalk is a JSON object, loaded into the global namespace as "dc_laws".
+    // At cite time, this function does a quick lookup in that crosswalk and links
+    // the user to the act which became the cited law.
+    function dcLawCited(cite) {
+        var lawNumber = cite.dc_law.period + "-" + cite.dc_law.number;
+        var prefixedLawNumber = cite.dc_law.period + "-" + zeroPrefix(cite.dc_law.number, 3);
+        console.log("lawNumber: " + lawNumber);
+        console.log("prefixedLawNumber: " + prefixedLawNumber);
+
+        var billNumber = dc_laws[prefixedLawNumber];
+        if (!billNumber) // leave unlinked
+            return cite.match;
+
+        var pieces = billNumber.split("-");
+        billNumber = pieces[0] + "-" + zeroPrefix(pieces[1], 4);
+
+        var text = "D.C. Law " + lawNumber;
+        var url = "http://dcclims1.dccouncil.us/lims/legislation.aspx?LegNo=B" + billNumber;
+        return linked(url, text);
+    }
+
+    // what in Ruby would be:
+    // "0" * 5 => "00000"
+    function repeated(num, string) {
+        return new Array(num + 1).join(string);
+    }
+
+    // handles a max of 4
+    function zeroPrefix(number, zeroes) {
+        var toAdd;
+        if (number < 10)
+            toAdd = zeroes - 1;
+        else if (number < 100)
+            toAdd = zeroes - 2;
+        else if (number < 1000)
+            toAdd = zeroes - 3;
+        else
+            toAdd = zeroes - 4;
+
+        if (toAdd < 0) toAdd = 0;
+
+        return repeated(toAdd, "0") + number;
     }
 
     function statCited(cite) {
